4d747100a2506b1a981d1dab47481c45b98ddede
23it018-cyber <23it018@psr.edu.in>
2026-01-16 21:39:35 +0530

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function login() {
-  window.location.href = "dashboard.html";
-}
-
-function saveAttendance() {
-  alert("Attendance Saved Successfully!");
-  window.location.href = "attendance-history.html";
-}
